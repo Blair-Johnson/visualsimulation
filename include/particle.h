af60195a917317ea866e3c6d37929a054e2bd417
bjohnson361 <blairjohnson@gatech.edu>
2021-04-10 14:42:30 +0200

    work on drawable particle textures

--- include/particle.h
+++ include/particle.h
@@ -1,27 +1,65 @@
 #pragma once
 #include <Eigen/Dense>
 #include <SDL.h>
+#include <cmath>
 
+
+struct Color {
+public:
+	Color(Uint8, Uint8, Uint8, Uint8);
+	Uint32 red;
+	Uint32 green;
+	Uint32 blue;
+	Uint32 alpha;
+};
 
 class Object {
 public:
+	Object();
+	virtual ~Object();
+	void setRenderer(SDL_Renderer*);
+	SDL_Renderer* getRenderer();
+	void setBounds(SDL_Rect);
+	SDL_Rect getBounds();
+	void setTexture(SDL_Texture*);
+	SDL_Texture* getTexture();
 	virtual bool render();
+
+public:
 	void setPos(Eigen::Vector2f);
 	void setVel(Eigen::Vector2f);
 	void setMass(float);
+	void setFnet(Eigen::Vector2f);
+	Eigen::Vector2f getFnet();
 	Eigen::Vector2f getPos();
 	Eigen::Vector2f getVel();
 	float getMass();
-public:
+
+private:
+	SDL_Renderer* m_renderer;
+	SDL_Texture* m_texture;
+	SDL_Rect m_bbox;
+
 	Eigen::Vector2f m_pos;
 	Eigen::Vector2f m_vel;
+	Eigen::Vector2f m_fnet;
 	float m_m;
 };
 
 
 class Particle: public Object {
 public:
-	Particle(Eigen::Vector2f, Eigen::Vector2f, float);
+	Particle(SDL_Renderer*, Eigen::Vector2f, Eigen::Vector2f, float, int);
+	void setColor(Uint8, Uint8, Uint8, Uint8);
+	bool render();
+	void setRadius(int);
+	int getRadius();
+
 private:
+	void computeBounds();
+	void update();
+	void step(float);
+	int m_rad;
+	Color m_color;
 
 };
--- src/particle.cpp
+++ src/particle.cpp
@@ -1,6 +1,13 @@
 #include "../include/particle.h"
 
 
+Color::Color(Uint8 p_r, Uint8 p_g, Uint8 p_b, Uint8 p_a) {
+	red = p_r;
+	green = p_g;
+	blue = p_b;
+	alpha = p_a;
+}
+
 Object::Object() {};
 
 float Object::getMass() {
@@ -15,6 +22,12 @@ Eigen::Vector2f Object::getVel() {
 	return m_vel;
 };
 
+SDL_Renderer* Object::getRenderer() {
+	return m_renderer;
+};
+
+bool Object::render() {};
+
 void Object::setMass(float p_m) {
 	m_m = p_m;
 };
@@ -27,10 +40,147 @@ void Object::setVel(Eigen::Vector2f p_vel) {
 	m_vel = p_vel;
 };
 
-Particle::Particle(Eigen::Vector2f p_pos, Eigen::Vector2f p_vel, float p_m) 
+void Object::setFnet(Eigen::Vector2f p_fnet) {
+	m_fnet = p_fnet;
+}
+
+Eigen::Vector2f Object::getFnet() {
+	return m_fnet;
+}
+
+void Object::setRenderer(SDL_Renderer* p_renderer) {
+	m_renderer = p_renderer;
+}
+
+void Object::setBounds(SDL_Rect p_bbox) {
+	m_bbox = p_bbox;
+}
+
+SDL_Rect Object::getBounds() {
+	return m_bbox;
+}
+
+void Object::setTexture(SDL_Texture* p_texture) {
+	m_texture = p_texture;
+}
+
+SDL_Texture* Object::getTexture() {
+	return m_texture;
+}
+
+Particle::Particle(SDL_Renderer* p_renderer, Eigen::Vector2f p_pos, Eigen::Vector2f p_vel, float p_m, int p_rad)
 {
 	setPos(p_pos);
 	setVel(p_vel);
 	setMass(p_m);
+	setRadius(p_rad);
+	setColor(0, 204, 153, 255);
+
+	computeBounds();
+	update();
 }
 
+void Particle::setRadius(int p_rad) {
+	m_rad = p_rad;
+}
+
+int Particle::getRadius() {
+	return m_rad;
+}
+
+bool Particle::render() {
+	return false;
+}
+
+void Particle::step(float dt) {
+	//physics goes here
+	Eigen::Vector2f dv = getFnet()/getMass() * dt;
+	setVel(getVel() + dv);
+	setPos(getPos() + getVel() * dt);
+}
+
+void Particle::update() {
+	Uint32 rmask, gmask, bmask, amask;
+
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	rmask = 0xff000000;
+	gmask = 0x00ff0000;
+	bmask = 0x0000ff00;
+	amask = 0x000000ff;
+	Uint32 pixColor = (m_color.red << 24) + (m_color.green << 16) + (m_color.blue << 8) + m_color.alpha;
+#else
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = 0xff000000;
+	Uint32 pixColor = (m_color.alpha << 24) + (m_color.blue << 16) + (m_color.green << 8) + m_color.red;
+#endif
+
+	// create texture
+	SDL_Surface* t_surface = SDL_CreateRGBSurface(0, getBounds().w, getBounds().h, 32, rmask, gmask, bmask, amask);
+	setTexture(SDL_CreateTextureFromSurface(getRenderer(), t_surface));
+	SDL_FreeSurface(t_surface);
+
+	// allocate pixel buffer
+	Uint32* t_pixels = new Uint32[getBounds().w * getBounds().h];
+
+	// clear pixel buffer
+	memset(t_pixels, 0, getBounds().w * getBounds().h * sizeof(Uint32));
+
+	// draw a circle
+	int cx = getPos()[0] - getBounds().x;
+	int cy = getPos()[1] - getBounds().y;
+	int r_squared = pow(getRadius(), 2);
+
+	for (int i = 0; i <= getBounds().w / 2; ++i) {
+		for (int j = 0; j <= getBounds().h / 2; ++j) {
+			int cVal = pow(i - cx, 2) + pow(j - cy, 2) - r_squared;
+			if (cVal <= 0) {
+				t_pixels[(j * getBounds().w) + i] = pixColor;
+				t_pixels[(j * getBounds().w) + ((getBounds().w - 1) - i)] = pixColor;
+				t_pixels[(((getBounds().h - 1) - j) * getBounds().w) + i] = pixColor;
+				t_pixels[(((getBounds().h - 1) - j) * getBounds().w) + ((getBounds().w - 1) - i)] = pixColor;
+			}
+		}
+	}
+
+	// update the texture with pixel buffer
+	SDL_UpdateTexture(getTexture(), NULL, t_pixels, getBounds().w * sizeof(Uint32));
+
+	// delete pixel buffer
+	delete[] t_pixels;
+}
+
+bool Particle::render() {
+	if (getRenderer() != nullptr) {
+		// blit texture into the renderer at the appropriate position
+		SDL_Rect srcRect; // basically window rect
+		srcRect.x = 0;
+		srcRect.y = 0;
+		srcRect.w = getBounds().w;
+		srcRect.h = getBounds().h;
+
+		SDL_RenderCopy(getRenderer(), getTexture(), &srcRect, &getBounds());
+
+		return true;
+	}
+	else
+		return false;
+}
+
+void Particle::computeBounds() {
+	int left = getPos()[0] - getRadius();
+	int top = getPos()[1] - getRadius();
+	int width = 2 * getRadius();
+	int height = 2 * getRadius();
+	SDL_Rect t_bounds = getBounds();
+	t_bounds.x = left;
+	t_bounds.y = top;
+	t_bounds.w = width;
+	t_bounds.h = height;
+	setBounds(t_bounds);
+}
+
+void Particle::setColor(Uint8 p_r, Uint8 p_g, Uint8 p_b, Uint8 p_a) {
+	m_color = Color(p_r, p_g, p_b, p_a);
+}
