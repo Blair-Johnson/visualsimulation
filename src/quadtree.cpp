62ef2f6450afa2d16cbe0dabcc8042ca6922ab0a
bjohnson361 <blairjohnson@gatech.edu>
2021-04-11 18:12:26 +0200

    Work on tree node

--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -1,9 +1,9 @@
 #include "../include/quadtree.h"
 
-TreeNode::TreeNode(std::vector<Particle*> p_particles) {
-	m_particles = p_particles;
+TreeNode::TreeNode(std::vector<Particle*> p_particles, float p_xmin, float p_xmax, float p_ymin, float p_ymax)
+	:m_particles(p_particles), xmin(p_xmin), xmax(p_xmax), ymin(p_ymin), ymax(p_ymax), m_mass(0)
+{
 	m_com = Eigen::Vector2f(0, 0);
-	m_mass = 0;
 	float t_mass = 0;
 
 	for (int i = 0; i < m_particles.size(); i++) {
